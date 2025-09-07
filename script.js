f47985c376de98753499f24716404f07da8084a2
Marcellin Barbeau <117610671+mbarbeau-umich@users.noreply.github.com>
2025-09-07 18:55:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+// TEST
+function exportToExcel() {
+  let data = [
+    ["Option", "Value"],
+    ["Choice 1", document.getElementById("option1").value]
+  ];
+  
+  let csv = data.map(row => row.join(",")).join("\n");
+  let blob = new Blob([csv], { type: "text/csv" });
+  let url = URL.createObjectURL(blob);
+  
+  let a = document.createElement("a");
+  a.href = url;
+  a.download = "export.csv";
+  a.click();
+}
